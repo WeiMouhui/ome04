80ba461275622b83720cc0565c81dd49a97ab8f4
Long Wenzhang <1280310226@qq.com>
2018-11-05 20:39:44 +0800

    提交

---
+++ git_test/src/cn/itheima/test.java
@@ -0,0 +1,10 @@
+package cn.itheima;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(234);
+        System.out.println(345);
+        
+    }
+}
